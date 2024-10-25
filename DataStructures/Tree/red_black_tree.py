676cfc0f2f20bb6a53c4ee52f2b3b4e8e362ebb6
sjimero <s.jimenezr2@uniandes.edu.co>
2024-10-25 08:35:48 -0500

    agregar funciones que en teoría deberían servir

--- DataStructures/Tree/red_black_tree.py
+++ DataStructures/Tree/red_black_tree.py
@@ -24,3 +24,184 @@ def insert_node(root, key, value):
     if root["right"] is None and root["left"] is not None:
         root["size"] = 1 + root["left"]["size"]
     return root
+
+#funciones que en teoría deberían funcionar
+def new_map():
+    mapa = {'root': None,
+     'type': "BST"}
+    return mapa
+
+def get(my_bst, key):
+    valor = recorrido_get(my_bst['root'], key)
+    return valor
+
+def recorrido_get(nodo, key):
+    llave = rbt_node.get_key(nodo)
+    if nodo != None:
+        if key == llave:
+            return rbt_node.get_value(nodo)
+        elif key > llave:
+            return recorrido_get(nodo['right'],key)
+        else:
+            return recorrido_get(nodo['left'],key)
+    else:
+        return None
+    
+def contains(my_bst, key):
+    contain = recorrido_con(my_bst['root'], key)
+    return contain
+
+def recorrido_con(nodo, key):
+    llave = rbt_node.get_key(nodo)
+    if nodo != None:
+        if key == llave:
+            return True
+        elif key > llave:
+            return recorrido_get(nodo['right'],key)
+        else:
+            return recorrido_get(nodo['left'],key)
+    else:
+        return False
+
+def size(my_bst):
+    if my_bst['root'] is None:
+        size = 0
+    else: 
+        size = my_bst['root']['size']
+    return size
+
+def is_empty(my_bst):
+    vacia = False
+    if my_bst['root'] is None:
+        vacia = True
+    return vacia
+
+def key_set(my_bst):
+    key_list = al.new_list()
+    if is_empty != True:
+        root = my_bst['root']
+        recursive_search_key(root,key_list)          
+    return key_list
+
+def recursive_search_key(nodo,lista):
+    if nodo != None:
+        recursive_search_key(nodo['left'],lista)
+        al.add_last(lista, nodo['key'])
+        recursive_search_key(nodo['right'],lista)
+        
+def value_set(my_bst):
+    value_list = al.new_list()
+    if is_empty != True:
+        root = my_bst['root']
+        recursive_search_value(root,value_list)
+    return value_list
+
+def recursive_search_value(nodo,lista):
+    if nodo != None:
+        recursive_search_value(nodo['left'],lista)
+        al.add_last(lista, nodo['value'])
+        recursive_search_value(nodo['right'],lista)
+
+def min_key(my_bst):
+    min_llave = min_key_node(my_bst['root'])
+    return min_llave
+
+def min_key_node(nodo):
+    if nodo is None:
+        return None
+    elif nodo['left'] is None:
+        return nodo['key']
+    else:
+        return min_key_node(nodo['left'])
+
+def max_key(my_bst):
+    min_llave = max_key_node(my_bst['root'])
+    return min_llave
+
+def max_key_node(nodo):
+    if nodo is None:
+        return None
+    elif nodo['right'] is None:
+        return nodo['key']
+    else:
+        return max_key_node(nodo['right'])
+    
+#ya hechas
+def rank(my_bst, key):
+    if my_bst["root"] == None:
+        return 0
+    else:
+        return recursive_rank(my_bst["root"], key)
+
+def recursive_rank(node, key):
+    if node == None:
+        return 0
+    elif node["key"] != key:
+        if node["key"] > key:
+            return recursive_rank(node["left"], key)
+        elif node["key"] < key:
+            return recursive_rank(node["right"], key)
+    else:
+        if node["left"] == None:
+            return 0
+        else:
+            return node["left"]["size"]
+
+def height(my_bst):
+    if my_bst == None:
+        return -1
+    else: 
+        nodo = my_bst.get("key")
+        if nodo == None: 
+            if my_bst["root"] == None:
+                return -1
+            else:
+                return recursive_height(my_bst["root"])
+        else:
+            return recursive_height(my_bst)
+
+def recursive_height(node):
+    if node == None:
+        return -1
+    else:
+        left_h = recursive_height(node["left"])
+        right_h = recursive_height(node["right"])
+        return 1 + max(left_h, right_h)
+
+def keys(my_bst, lo_key, hi_key):
+    key_list = al.new_list()
+    if my_bst != None:
+        if my_bst["root"] != None:
+            recursive_search_keys(my_bst["root"], lo_key, hi_key, key_list)
+    al.quick_sort(key_list, sort_crit)
+    return key_list
+
+def values(my_bst, lo_key, hi_key):
+    key_list = al.new_list()
+    if my_bst != None:
+        if my_bst["root"] != None:
+            recursive_search_values(my_bst["root"], lo_key, hi_key, key_list)
+    al.quick_sort(key_list, sort_crit)
+    return key_list
+
+def recursive_search_keys(nodo, lo_key, hi_key, lista):
+    if nodo != None:
+        if lo_key <= nodo["key"] <= hi_key:
+            al.add_last(lista, nodo["key"])
+            recursive_search_keys(nodo["right"], lo_key, hi_key, lista)
+            recursive_search_keys(nodo["left"], lo_key, hi_key, lista)
+        elif nodo["key"] > hi_key:
+            recursive_search_keys(nodo["left"], lo_key, hi_key, lista)
+        elif nodo["key"] < lo_key:
+            recursive_search_keys(nodo["right"], lo_key, hi_key, lista)
+
+def recursive_search_values(nodo, lo_key, hi_key, lista):
+    if nodo != None:
+        if lo_key <= nodo["value"] <= hi_key:
+            al.add_last(lista, nodo["value"])
+        recursive_search_values(nodo["right"], lo_key, hi_key, lista)
+        recursive_search_values(nodo["left"], lo_key, hi_key, lista)
+        
+
+def sort_crit(ele1, ele2):
+    return ele1 < ele2
